d9535b877e7cdc5f3bd14d087fb0d227a59d7ad6
geonil87 <geonil87@Mirage>
2017-10-11 14:39:03 +0900

    2017-10-11 14:39

--- Teatime/src/main/java/board/dto/StoryBoardDTO.java
+++ Teatime/src/main/java/board/dto/StoryBoardDTO.java
@@ -2,6 +2,7 @@ package board.dto;
 
 public class StoryBoardDTO {
 	
+
 	int idx;
 	String title;
 	String content;
@@ -13,16 +14,72 @@ public class StoryBoardDTO {
 	public StoryBoardDTO(){}
 	
 	public StoryBoardDTO(int idx, String title, String content, String user_nick, String wdate, int hit) {
-		super();
+		
+		
 		this.idx = idx;
 		this.title = title;
 		this.content = content;
 		this.user_nick = user_nick;
 		this.wdate = wdate;
 		this.hit = hit;
+		
+	
+	}
+	
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getUser_nick() {
+		return user_nick;
+	}
+
+	public void setUser_nick(String user_nick) {
+		this.user_nick = user_nick;
+	}
+
+	public String getWdate() {
+		return wdate;
+	}
+
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
 	}
 	
 	
+	@Override
+	public String toString() {
+		return "StoryBoardDTO [idx=" + idx + ", title=" + title + ", content=" + content + ", user_nick=" + user_nick
+				+ ", wdate=" + wdate + ", hit=" + hit + "]";
+	}
 	
 
 }
